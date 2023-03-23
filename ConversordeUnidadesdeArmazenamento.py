2256f614c1e83cb0293ce8d244331195a2089faf
Henrique Coelho Vargas <henriquecoelho.vargas@gmail.com>
2023-03-23 11:21:27 -0300

    Criei mais três funções para converter respectivamente: de Mbyte para Gbyte, de Gbyte para Tbyte, de Tbyte para Pbyte

--- ConversordeUnidadesdeArmazenamento.py
+++ ConversordeUnidadesdeArmazenamento.py
@@ -16,6 +16,22 @@ def ByteParaKByte(ValoremByte):
     return KbytesCalculado
 
 def KByteParaMByte(ValoremKByte):
-    print('Valor convertido de byte para Mbyte')
+    print('Valor convertido de Kbyte para Mbyte')
     MbytesCalculado = ValoremKByte / 1024
     return MbytesCalculado
+
+def MByteParaGByte(ValoremMByte):
+    print('Valor convertido de Mbyte para Gbyte')
+    GbytesCalculado = ValoremMByte / 1024
+    return GbytesCalculado
+
+def GByteParaTByte(ValoremGByte):
+    print('Valor convertido de Gbyte para Tbyte')
+    TbytesCalculado = ValoremGByte / 1024
+    return TbytesCalculado
+
+def TByteParaPByte(ValoremTByte):
+    print('Valor convertido de Tbyte para Pbyte')
+    PbytesCalculado = ValoremTByte / 1024
+    return PbytesCalculado
+
