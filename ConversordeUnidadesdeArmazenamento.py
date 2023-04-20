262c94b758c2ce3d641163adb62a3894228ecde3
Henrique Coelho Vargas <henriquecoelho.vargas@gmail.com>
2023-04-20 11:15:11 -0300

    Modifiquei o arquivo Conversor de unidades para fazer as operações de conversão corretamente

--- ConversordeUnidadesdeArmazenamento.py
+++ ConversordeUnidadesdeArmazenamento.py
@@ -1,13 +1,19 @@
 ListadeUnidades=['bit', 'byte', 'Kbyte', 'Mbyte', 'Gbyte', 'Tbyte', 'Pbyte']
 Numero=10
-UnidadeInicial= 'byte'
-UnidadeFinal= 'Mbyte'
+UnidadeInicial= 'Mbyte'
+UnidadeFinal= 'byte'
 
-def calcularConversao ():
+def calcularConversao (UnidadeInicial, UnidadeFinal, Numero):
     posicaoinicial=0
+    posicaofinal=0
+    variacaoDasUnidades=0
     for i in ListadeUnidades:
         if i == UnidadeInicial:
             posicaoinicial = ListadeUnidades.index(i)
-    return posicaoinicial
-
-print (calcularConversao())
+    for i in ListadeUnidades:
+        if i == UnidadeFinal:
+            posicaofinal = ListadeUnidades.index(i)
+    variacaoDasUnidades= posicaofinal-posicaoinicial
+    conversor= 1024**variacaoDasUnidades
+    resp= Numero / conversor
+    return resp
