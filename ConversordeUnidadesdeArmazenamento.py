45a8b27ed270bbf769a0d9ec9c518b5d3792bfb9
Henrique Coelho Vargas <henriquecoelho.vargas@gmail.com>
2023-04-20 13:36:17 -0300

    Adicionei a conversão de byte para o programa

--- ConversordeUnidadesdeArmazenamento.py
+++ ConversordeUnidadesdeArmazenamento.py
@@ -1,12 +1,18 @@
 ListadeUnidades=['bit', 'byte', 'Kbyte', 'Mbyte', 'Gbyte', 'Tbyte', 'Pbyte']
-Numero=10
-UnidadeInicial= 'Mbyte'
-UnidadeFinal= 'byte'
+Numero=80000
+UnidadeInicial= 'bit'
+UnidadeFinal= 'Mbyte'
 
 def calcularConversao (UnidadeInicial, UnidadeFinal, Numero):
     posicaoinicial=0
     posicaofinal=0
     variacaoDasUnidades=0
+    if UnidadeInicial == 'bit':
+        Numero /= 8
+        UnidadeInicial = 'byte'
+    if UnidadeFinal == 'bit':
+        Numero /= 8
+        UnidadeFinal = 'byte'
     for i in ListadeUnidades:
         if i == UnidadeInicial:
             posicaoinicial = ListadeUnidades.index(i)
@@ -15,5 +21,8 @@ def calcularConversao (UnidadeInicial, UnidadeFinal, Numero):
             posicaofinal = ListadeUnidades.index(i)
     variacaoDasUnidades= posicaofinal-posicaoinicial
     conversor= 1024**variacaoDasUnidades
-    resp= Numero / conversor
+    if variacaoDasUnidades > 0:
+        resp= Numero / conversor
+    if variacaoDasUnidades < 0:
+        resp= Numero * conversor
     return resp
